d4dc88912b4f4e03b1dbd997ef6452c3aa58de93
Michael Lawrence <mlawrenc@mathworks.com>
2024-08-07 18:29:33 -0400

    Adding Graphics controller to Enemy.

--- src/shmup/entities/enemy.lua
+++ src/shmup/entities/enemy.lua
@@ -1,19 +1,16 @@
 --- @class shmup.entities.Enemy
---- @field scale shmup.drawing.Point The scale of the image being rendered.
---- @field rotation number The rotation to render the image.
 --- @field canvas love.Canvas The canvas to draw the enemy on.
 --- @field blur shmup.shaders.Blur The blur shader to use when drawing the enemy.
---- @field image love.Image The image to draw the enemy on.
---- @field imageSize shmup.drawing.Point The size of the image being rendered.
 --- @field controllers table A table of all the controllers that are currently active.
 --- @field controllers.movement shmup.controllers.MovementController The movement controller.
 --- @field controllers.projectile shmup.controllers.ProjectileController The projectile controller.
+--- @field controllers.graphics shmup.controllers.GraphicsController The graphics controller.
 local P = {}
 
 local Point = require('shmup.drawing.point')
-local Rect = require('shmup.drawing.rect')
 local MovementController = require('shmup.controllers.movement')
 local ProjectileController = require('shmup.controllers.projectile')
+local GraphicsController = require('shmup.controllers.graphics')
 local Blur = require('shmup.shaders.blur')
 local G, W = love.graphics, love.window
 local windowW, windowH = W.getMode()
@@ -27,22 +24,21 @@ function P:new(defaults)
 
     local movementController = MovementController:new({ speed = 1 })
     local projectileController = ProjectileController:new({ imagePath = 'assets/beam.png' })
+    local graphicsController = GraphicsController:new({
+        imagePath = defaults.imagePath,
+        scale = defaults.scale or Point:new({ x = 1, y = 1 }),
+        rotation = defaults.rotation or 0,
+    })
 
     local o = {
         controllers = {
             movement = movementController,
             projectile = projectileController,
+            graphics = graphicsController,
         },
-        scale = Point:new({ x = 1, y = 1 }),
-        rotation = 0,
         canvas = G.newCanvas(),
         blur = Blur:new(),
-        image = G.newImage(defaults.imagePath),
     }
-
-    local imageW, imageH = o.image:getDimensions()
-    o.imageSize = Point:new({ x = imageW, y = imageH })
-    o.image:setFilter('nearest', 'linear')
 
     setmetatable(o, self)
     self.__index = self
@@ -55,14 +51,16 @@ end
 --- @param self shmup.entities.Enemy The enemy instance.
 --- @param dt number The time since the last update, in seconds.
 function P:update(dt)
+    local graphics = self.controllers.graphics
     local movement = self.controllers.movement
-    local enemySize = self:getSize()
-    movement:setSize(enemySize)
+    local size = graphics:getSize()
+    movement:setSize(size)
 
     local position = movement.position
+    graphics:setPosition(position)
     self.blur:update(position)
 
-    local projectileOffset = Point:new({ x = enemySize.x * 0.27, y = 0 })
+    local projectileOffset = Point:new({ x = size.x * 0.27, y = 0 })
     local projectile = self.controllers.projectile
     projectile:setPosition(position + projectileOffset)
     projectile:update(dt)
@@ -70,13 +68,13 @@ end
 
 --- Draws the enemy on the screen at their current position, rotation, and scale.
 function P:draw()
-    local position = self.controllers.movement.position
+    local graphics = self.controllers.graphics
     local oldCanvas = G.getCanvas()
     G.setCanvas(self.canvas)
     G.clear()
 
     self.blur:draw()
-    G.draw(self.image, position.x, position.y, self.rotation, self.scale.x, self.scale.y)
+    graphics:draw()
     self.blur:reset()
 
     self.controllers.projectile:draw()
@@ -88,10 +86,10 @@ end
 --- Initializes the enemy's starting position on the screen.
 --- This function sets the enemy's position to be centered horizontally and at the bottom of the screen.
 function P:init()
-    local enemySize = self:getSize()
+    local size = self.controllers.graphics:getSize()
     local position = Point:new({
-        x = (windowSize.x / 2) - (enemySize.x / 2),
-        y = -enemySize.y
+        x = (windowSize.x / 2) - (size.x / 2),
+        y = -size.y
     })
 
     self:setPosition(position)
@@ -101,43 +99,6 @@ end
 --- @param position shmup.drawing.Point The new position of the enemy.
 function P:setPosition(position)
     self.controllers.movement:setPosition(position)
-end
-
----Sets the scale of the enemy.
---- @param scale shmup.drawing.Point The new scale of the enemy.
-function P:setScale(scale)
-    self.scale:setPoint(scale)
-end
-
---- Sets the rotation of the enemy.
---- @param rotation number The new rotation of the enemy, in radians.
-function P:setRotation(rotation)
-    self.rotation = rotation
-end
-
----Sets the speed of the enemy.
---- @param speed number The new speed of the enemy.
-function P:setSpeed(speed)
-    self.controllers.movement:setSpeed(speed)
-end
-
---- Returns the width and height of the enemy's image, scaled by the enemy's scale.
---- @return shmup.drawing.Point point The size of the enemy's image, scaled.
-function P:getSize()
-    return self.imageSize * self.scale
-end
-
---- Returns a rectangle representing the enemy's position and size.
---- @return shmup.drawing.Rect The rectangle representing the enemy's position and size.
-function P:getRect()
-    local position = self.controllers.movement.position
-
-    return Rect:new({
-        x = position.x,
-        y = position.y,
-        w = self.imageSize.x,
-        h = self.imageSize.y,
-    })
 end
 
 --- Moves the enemy left by the enemy's speed.
@@ -158,6 +119,10 @@ end
 --- Moves the enemy down by the enemy's speed.
 function P:moveDown()
     self.controllers.movement:down()
+end
+
+function P:getRect()
+    return self.controllers.graphics:getRect()
 end
 
 return P
